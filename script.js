ed96fdc4cc333c2d566180c8830c96ef174476fc
AbhishekSuraj0 <ab8948464510gmmail.com>
2026-08-30 19:32:47 +0530

    gg

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const sh = "1UGM3yaIqoa0UK9bKl-nEDAE5UoNQggWKDORZ64AR8qA";
+var catagoriesdata = document.getElementById("catagoriesdata")
 
-fetch(`https://opensheet.elk.sh/${sh}/product`)
-  .then(res => res.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(err => {
-    console.error("Error:", err);
-  });
+
+
+
+
+
+var catagories = document.querySelector(".catagories");
+
+catagories.addEventListener("click", (e) => {
+    fetch(`https://opensheet.elk.sh/${sh}/product`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(err => {
+            console.error("Error:", err);
+        });
+});
